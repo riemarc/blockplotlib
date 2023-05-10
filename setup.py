2cbf9789af5b430cf4ae184427669f3127b96563
Marcus Riesmeier <riemarc@users.noreply.github.com>
2023-05-10 22:06:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import setuptools
 
-description = ("Matplotlib-based library for block diagram creation.")
+description = "Matplotlib-based library for the creation of block diagrams"
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
+long_description = read_file("readme.md")
+requirements = read_file("requirements.txt").splitlines()
 
 setuptools.setup(
     name="blockplotlib",
-    version="2020.1a",
+    version="2023.1.1",
     author="Marcus Riesmeier",
-    author_email="marcus.riesmeier@umit.com",
+    author_email="gluehen-sierren-0c@icloud.com",
     license="BSD 3-Clause License",
     description=description,
+    long_description_content_type="text/markdown",
     long_description=description,
     packages=setuptools.find_packages(),
     install_requires=requirements,
